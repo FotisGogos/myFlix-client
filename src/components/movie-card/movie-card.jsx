c3dec29f3e1365ddc34fc2e24e32ae8fbeac12fc
Fotis Gogos <83247314+FotisGogos@users.noreply.github.com>
2021-09-09 00:19:40 +0200

    prop-types is imported and propTypes values are added too

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick } = this.props;
 
-    return <div className="movie-card" onClick={() => { onMovieClick(movie); }} >{movie.Title}
+    return <div className="movie-card" onClick={() => { onMovieClick(movie); }} >{movie.title}
           </div>;
   }
 }
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+    })
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};

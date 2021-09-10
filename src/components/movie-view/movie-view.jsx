41c44fd88b0180849457061958d953c639552cb6
Fotis Gogos <83247314+FotisGogos@users.noreply.github.com>
2021-09-10 21:02:31 +0200

    Update movie view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./movie-view.scss";
 
 export class MovieView extends React.Component {
   render(){
@@ -16,6 +17,14 @@ export class MovieView extends React.Component {
         <div className="movie-description">
           <span className="label">Description: </span>
           <span className="value">{movie.Description}</span>
+        </div>
+        <div className="movie-genre">
+          <span className="label">Genre: </span>
+          <span className="value">{movie.Genre}</span>
+        </div>
+        <div className="movie-director">
+          <span className="label">Director: </span>
+          <span className="value">{movie.Director}</span>
         </div>
           <button onClick={() => { onBackClick(null); }}>Back</button>
       </div>

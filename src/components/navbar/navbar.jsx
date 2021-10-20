9ac3d5a0b4f544cc158b8538eaa419bc0fa93546
Fotis Gogos <83247314+FotisGogos@users.noreply.github.com>
2021-10-20 23:49:21 +0200

    3.6 New navbar + sign up is added

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-//import logo from '../../public/MovieExperts.png';
+import { Navbar, Container, Nav,Button, } from 'react-bootstrap';
+import { Link} from 'react-router-dom';
 
  export function Navbar (props)  { 
-
-  const { logo } = props; 
-  
-  
+    
   onLoggedOut = () => {
     localStorage.clear();
     window.open("/", "_self");
@@ -15,24 +11,28 @@ import { Link } from 'react-router-dom';
 
   return (
  
-    <Navbar className="nav-main">
-    <Container>
-      <Navbar.Brand href="#home"> <img src ={logo} alt="myFlix logo" /> Logo</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-      <Navbar.Collapse className="justify-content-end">
-      <Nav.Link href="/Login">Log in </Nav.Link>
-      <Link to={"/register"}>
-            <Button variant="primary" className="signUp-link" type="submit">Sign up</Button>
+  <Navbar className="main-nav" sticky="top" bg="dark" expand="lg" variant="dark">
+    <Container> 
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="ml-auto">   
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/profile">Profile</Nav.Link>
+          <Nav.Link href="/editProfile">Update your account</Nav.Link>
+          <Button variant="link" onClick={() => { this.onLoggedOut() }}>Logout</Button>
+          <Link to="/register">
+            <Button variant="primary" className="signUp-link" >Sign up</Button>
           </Link>
-
-      <Button variant="link" onClick={() => { this.onLoggedOut() }}>Logout</Button>
-          
-        
+        </Nav>
       </Navbar.Collapse>
     </Container>
-  </Navbar>
+</Navbar>
+    );
+  }
+ 
 
-  )
+
+ 
+
+
   
-}
-

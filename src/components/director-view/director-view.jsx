bf21136011b247c22e3b1f2240c477516329c759
Fotis Gogos <83247314+FotisGogos@users.noreply.github.com>
2021-11-25 19:46:41 +0100

    3.8 Card Bootstrap

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,32 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Row, Col, Button } from 'react-bootstrap';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Card, Container, Button } from "react-bootstrap";
 
- export function DirectorView (props)  {
-  const { director } = props; 
-  console.log (director);
+export function DirectorView(props) {
+  const { director } = props;
+
   return (
-    <div>
-     <div className="director-name">
-          <span className="label">Name: </span>
-          <span className="value">{director.Name}</span> 
-             
-        </div>
-        <div className="director-bio">
+    <Container className=" director-container">
+      <Card>
+        <Card.Header>
+          <span className="value">{director.Name}</span>
+        </Card.Header>
+        <Card.Header>
           <span className="label">Biography: </span>
-          <span className="value">{director.Bio}</span>     
-        </div>  
-        <div className="director-birthyear">
+          <Card.Text>
+            <span className="value">{director.Bio}</span>
+          </Card.Text>
+        </Card.Header>
+        <Card.Header>
           <span className="label">Year of Birth: </span>
-          <span className="value">{director.Born}</span>      
-        </div>
+          <Card.Text>
+            <span className="value">{director.Born}</span>
+          </Card.Text>
+        </Card.Header>
+
         <Link to={`/`}>
-                <Button className='returnButton' variant='dark'>Return to Movie List</Button>
-              </Link>
-              
-      
-      </div>
-       
-  )}
-  
-  
+          <Button className="returnButton" variant="outline-secondary">
+            Return to Movie List
+          </Button>
+        </Link>
+      </Card>
+    </Container>
+  );
+}
